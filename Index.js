e597cd5c1a4faf13f1641b508d548468334ea4fe
Beena Evangelin <graciaevangelin99@gmail.com>
2023-10-10 23:21:52 +0530

    Practised a few pattern problems

--- Index.js
+++ Index.js
@@ -2683,3 +2683,98 @@ const hollowPyramidPatt = function () {
   console.log(string);
 };
 hollowPyramidPatt();
+
+const diamondPatt = function () {
+  let n = 5;
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      string += " ";
+    }
+    for (let k = 0; k < 2 * i - 1; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  for (let i = 1; i <= n - 1; i++) {
+    for (let j = 0; j < i; j++) {
+      string += " ";
+    }
+    for (let k = 0; k < 2 * (n - i) - 1; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
+
+diamondPatt();
+
+const hourglassPatt = function () {
+  let n = 5;
+  let string = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      string += " ";
+    }
+    for (let k = 0; k < 2 * (n - i) - 1; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  for (let i = 2; i <= n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      string += " ";
+    }
+    for (let k = 0; k < 2 * i - 1; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
+hourglassPatt();
+
+const rightPascalPatt = function () {
+  let n = 5;
+  let string = "";
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
+rightPascalPatt();
+
+const leftPascalPatt = function () {
+  let n = 5;
+  let string = "";
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      string += " ";
+    }
+    for (let k = 0; k < i; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      string += " ";
+    }
+    for (let k = 0; k < n - i; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
+leftPascalPatt();
