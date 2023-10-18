cb46b5e2bcdc1eeb0187d7d5fe7fd4f5b6caa8b1
Beena Evangelin <graciaevangelin99@gmail.com>
2023-10-19 00:55:16 +0530

    Worked on a problem

--- Index.js
+++ Index.js
@@ -3120,3 +3120,18 @@ const findMaxandMin1 = function (arr) {
   return `${maximum} ${minimum}`;
 };
 console.log(findMaxandMin1(givenArr3));
+
+const triangleNumPatt = function () {
+  let n = 4;
+  let string = "";
+  let count = 1;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      string += count;
+      count++;
+    }
+    string += " \n";
+  }
+  console.log(string);
+};
+triangleNumPatt();
