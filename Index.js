b9360788eb4b7f934fbcb3aa42f6fd97caaf4c57
Beena Evangelin <graciaevangelin99@gmail.com>
2023-10-21 23:26:40 +0530

    practised a couple of number pattern problem

--- Index.js
+++ Index.js
@@ -3174,3 +3174,16 @@ const invertTriangleDecrementPatt = function () {
   console.log(string);
 };
 invertTriangleDecrementPatt();
+
+const invertTriangleDecrementPatt2 = function () {
+  let n = 5;
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i + 1; j++) {
+      string += n - j + 1;
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
+invertTriangleDecrementPatt2();
