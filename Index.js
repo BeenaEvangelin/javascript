3581abc54c2cfab5adc4ff025325c9cbdc19da23
Beena Evangelin <graciaevangelin99@gmail.com>
2023-10-21 23:21:06 +0530

    Practised a problem

--- Index.js
+++ Index.js
@@ -3161,3 +3161,16 @@ const invertTriReverseNumPatt = function () {
   console.log(string);
 };
 invertTriReverseNumPatt();
+
+const invertTriangleDecrementPatt = function () {
+  let n = 5;
+  let string = "";
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n - i + 1; j++) {
+      string += n - j - i + 2;
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
+invertTriangleDecrementPatt();
